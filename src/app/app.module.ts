433703b1d6e154195ce0cfe9a3ec77bb5f869c8e
Abhishek Kumar Yadav <abhiy.yadav7@gmail.com>
2018-11-14 15:25:58 +0530

    adding other components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,16 @@ import {OnboardingComponent} from './onboarding/onboarding.component';
 import { HomeComponent } from './home/home.component';
 import {ReactiveFormsModule} from '@angular/forms';
 import {HttpClientModule} from '@angular/common/http';
+import { CockpitComponent } from './cockpit/cockpit.component';
+import { ServerElementComponent } from './server-element/server-element.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     OnboardingComponent,
-    HomeComponent
+    HomeComponent,
+    CockpitComponent,
+    ServerElementComponent
   ],
   imports: [
     BrowserModule,
